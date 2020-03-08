4f728fdaf6d53013bacb61d748a88463033fff8a
kidonKim <ilhvm12@naver.com>
2020-03-08 23:39:51 +0900

    Updates

---
+++ precache-manifest.4bd4c0053dc83be819f7a69ded137f75.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5800abf5194d80782a3dd296fba9609",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "d5442741d53930f845e1",
+    "url": "/movie_app/static/css/main.12a45380.chunk.css"
+  },
+  {
+    "revision": "7487e8bdbdd89aef3808",
+    "url": "/movie_app/static/js/2.0a2bb4f9.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/movie_app/static/js/2.0a2bb4f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d5442741d53930f845e1",
+    "url": "/movie_app/static/js/main.cb896687.chunk.js"
+  },
+  {
+    "revision": "ca4b155c3570fabe1dd4",
+    "url": "/movie_app/static/js/runtime-main.98d66c69.js"
+  }
+]);
